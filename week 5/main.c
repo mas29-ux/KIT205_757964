21f3af226b4eea38891a03494fe629e0303203cc
unknown <mas29@utas.edu.au>
2026-04-08 05:17:45 -0700

    code for week 5 main.c

---
+++ week 5/main.c
@@ -0,0 +1,12 @@
+#include "bst.h"
+
+int main() {
+
+    // run the built-in BST test suite to check functionality
+    bst_test();
+
+    // remove the comment below if you want to test the BST by entering values manually
+    // bst_adhoc_test();
+
+    return 0;
+}
